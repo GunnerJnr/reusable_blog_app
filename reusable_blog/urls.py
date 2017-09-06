399daf00e8a438948d4d5ae809408b49b5950b94
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-09-06 14:39:32 +0100

    typo fix up

--- reusable_blog/urls.py
+++ reusable_blog/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^blog/$', views.post_list, name='post-list'),
+    url(r'^/$', views.post_list, name='post-list'),
     url(r'^(?P<slug>[-\w]+)$', views.post_detail, name='post-detail'),
     url(r'^top-five/$', views.display_top_five_posts, name='top-five'),
     url(r'^post/new/$', views.new_post, name='new_post'),
