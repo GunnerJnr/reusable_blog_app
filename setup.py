49e5e27252389199396f697b29de056315460cba
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-03 16:35:47 +0100

    update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
  
 setup(
     name='reusable-blog-app',
-    version='1.0.1',
+    version='1.0.2',
     packages=['reusable_blog'],
     include_package_data=True,
     license='BSD License',  # example license
